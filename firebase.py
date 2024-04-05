1a4531dde102cdaf25226598389331da74cad450
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2024-04-05 23:22:44 +0000

    Update create hacker

--- firebase.py
+++ firebase.py
@@ -7,11 +7,11 @@ firebase_admin.initialize_app(cred)
 
 # Create a new document in Firestore
 def create_document(collection : str, document_data: dict):
-    db = firestore.client()
-    doc_ref = db.collection(collection).document()
-    doc_ref.set(document_data)
-    print('Document created with ID:', doc_ref.id)
-
-# Usage example
-create_document('users', {'name': 'John Doe', 'email': 'johndoe@example.com'})
+    try:
+        db = firestore.client()
+        doc_ref = db.collection(collection).document()
+        doc_ref.set(document_data)
+        print('Document created with ID:', doc_ref.id)
+    except Exception as e:
+        print('An error occurred:', e)
 
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from typing import Union
 from fastapi import FastAPI
-
+from firebase import create_document
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/add_hacker")
+def add_hacker(name: str, ghw: str, status: str):
+    data = {
+        "name": name,
+        "ghw": ghw,
+        "status": status
+    }
+    create_document("hackers", data)
+    return {"status": "success"}
